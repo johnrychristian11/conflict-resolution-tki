dfd50b3bad6472df81fdbe40e42ccacd22aa4d2c
johnrychristian <johnrychristian@gmail.com>
2025-11-18 08:59:26 -0500

    sasas

--- input2.py
+++ input2.py
@@ -3,24 +3,20 @@ SAMPLE_CONVERSATION = [
         'person': 'Customer',
         'text': "I need a refund for my order. It arrived late and I don't want it anymore.",
         'audio': 'C:/Users/Johnr/Music/scenario_2/1.mp3'
-        #Expected_TKI': 'Competing'
     },
     {
         'person': 'Support Agent',
         'text': "I understand your frustration. Let's see if we can find a solution that works for both of us.",
         'audio': 'C:/Users/Johnr/Music/scenario_2/2.mp3'
-        #'Expected_TKI': 'Collaborating'
     },
     {
         'person': 'Customer',
         'text': "Store credit is fine. I just want a quick resolution.",
         'audio': 'C:/Users/Johnr/Music/scenario_2/3.mp3'
-        #'Expected_TKI': 'Compromising'
     },
     {
         'person': 'Support Agent',
         'text': "Ok let us do that",
         'audio': 'C:/Users/Johnr/Music/scenario_2/4.mp3'
-        #'Expected_TKI': 'Accommodating'
     }
 ]
